16b0984710857766e708e435d91bd0369be0a2ae
Florian Sprenger <florian.sprenger@datev.de>
2024-03-14 23:41:08 +0100

    initial commit

---
+++ src/main/java/de/dhbw/trackingappbackend/TrackingAppBackendApplication.java
@@ -0,0 +1,13 @@
+package de.dhbw.trackingappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackingAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/dhbw/trackingappbackend/TrackingAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dhbw.trackingappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
