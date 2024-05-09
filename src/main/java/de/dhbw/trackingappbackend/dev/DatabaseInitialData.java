0b2d481f2c4838bc8556dba9c950071d55802545
Florian Sprenger <floriansprenger27@gmail.com>
2024-05-09 20:14:40 +0200

    added permanent test data

--- src/main/java/de/dhbw/trackingappbackend/dev/DatabaseInitialData.java
+++ src/main/java/de/dhbw/trackingappbackend/dev/DatabaseInitialData.java
@@ -33,40 +33,69 @@ public class DatabaseInitialData {
             fileRepository.deleteAll();
 
             String password = encoder.encode("password123.");
+            String uuid1 = UUID.randomUUID().toString();
+            String uuid2 = UUID.randomUUID().toString();
+            String uuid3 = UUID.randomUUID().toString();
+            String uuid4 = UUID.randomUUID().toString();
 
-            AppUser appUser1 = new AppUser(
-                    UUID.randomUUID().toString(),
-                    "firstname1",
-                    "lastname1",
-                    "datev@test.de",
-                    "username1",
-                    password,
-                    List.of(new Friend("1",Friend.open, "email@email.de",
-                                    Instant.now(), Instant.now()),
-                            new Friend("2",Friend.accepted, "email2@email.de",
-                                    Instant.now(), Instant.now()),
-                            new Friend("3",Friend.accepted, "email2@email.de11",
-                                    Instant.now(), Instant.now()),
-                             new Friend("4",Friend.accepted, "email2@email.de1212",
-                                    Instant.now(), Instant.now()),
-                            new Friend("5",Friend.accepted, "email2@email.de222",
-                                    Instant.now(), Instant.now())),
-                    Collections.emptyList(),
-                    null);
 
-            AppUser appUser2 = new AppUser(
-                    UUID.randomUUID().toString(),
-                    "firstname2",
-                    "lastname2",
-                    "test2@test.de",
-                    "username2",
-                    password,
-                    Collections.emptyList(),
-                    Collections.emptyList(),
-                    null);
+            AppUser appUser = AppUser.builder()
+                    .email("test1@test.de")
+                    .id(uuid1)
+                    .password(password)
+                    .username("test1Username")
+                    .firstname("TestFirstName")
+                    .lastname("TestLastName")
+                    .friends(List.of(
+                            new Friend(uuid2, Friend.accepted, "test2@test.de", Instant.now(), Instant.now()),
+                            new Friend(uuid3, Friend.accepted, "test3@test.de", Instant.now(), Instant.now()),
+                            new Friend(uuid4, Friend.open, "test4@test.de", Instant.now(), Instant.now())
+                            ))
+                    .locationIds(Collections.emptyList())
+                    .profilePictureId(null)
+                    .build();
 
+            AppUser appUser1 = AppUser.builder()
+                    .email("test2@test.de")
+                    .id(uuid2)
+                    .password(password)
+                    .username("test2Username")
+                    .firstname("Test2FirstName")
+                    .lastname("Test2LastName")
+                    .friends(Collections.emptyList())
+                    .locationIds(Collections.emptyList())
+                    .profilePictureId(null)
+                    .build();
+
+            AppUser appUser2 = AppUser.builder()
+                    .email("test3@test.de")
+                    .id(uuid3)
+                    .password(password)
+                    .username("test3Username")
+                    .firstname("Test3FirstName")
+                    .lastname("Test3LastName")
+                    .friends(Collections.emptyList())
+                    .locationIds(Collections.emptyList())
+                    .profilePictureId(null)
+                    .build();
+
+            AppUser appUserNoFriends = AppUser.builder()
+                    .email("test4@test.de")
+                    .id(uuid4)
+                    .password(password)
+                    .username("test4Username")
+                    .firstname("Test4FirstName")
+                    .lastname("Test4LastName")
+                    .friends(Collections.emptyList())
+                    .locationIds(Collections.emptyList())
+                    .profilePictureId(null)
+                    .build();
+
+
+            userRepository.save(appUser);
             userRepository.save(appUser1);
             userRepository.save(appUser2);
+            userRepository.save(appUserNoFriends);
         };
     }
 }
