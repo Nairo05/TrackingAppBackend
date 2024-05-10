4b9c873cb2052ac47dbeed18e188eef41b43a0b6
Florian Sprenger <floriansprenger27@gmail.com>
2024-05-10 13:27:54 +0200

    added email

---
+++ src/main/java/de/dhbw/trackingappbackend/control/EmailService.java
@@ -0,0 +1,23 @@
+package de.dhbw.trackingappbackend.control;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    public void sendSimpleMessage(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("i21026@outlook.de");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        mailSender.send(message);
+    }
+
+}
